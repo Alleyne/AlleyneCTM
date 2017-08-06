87e458fac5fb90a03ff74e8e3f494081941debd3
gabarriosb <gabarriosb@gmail.com>
2017-08-05 19:23:02 -0500

    datatables

--- resources/views/core/uns/indexunall.blade.php
+++ resources/views/core/uns/indexunall.blade.php
@@ -86,8 +86,8 @@
 
 @section('relatedplugins')
 
-    <script src="{{ URL::asset('assets/backend/js/datatables/jquery.dataTables-1.10.15.min.js') }}"></script>
-    {{-- <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script> --}}
+    {{-- <script src="{{ URL::asset('assets/backend/js/datatables/jquery.dataTables-1.10.15.min.js') }}"></script> --}}
+    <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
     <script>
         $(document).ready(function() {
          
