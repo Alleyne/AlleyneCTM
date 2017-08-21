f9107cb4b36b2c48bbd26ff25e78701598573124
gabarriosb <gabarriosb@gmail.com>
2017-08-21 11:44:41 -0500

    se eliminan algunos javascripts

--- resources/views/templates/backend/_partials/javascript.blade.php
+++ resources/views/templates/backend/_partials/javascript.blade.php
@@ -9,40 +9,7 @@
 		<!-- CUSTOM NOTIFICATION -->
 		<script src="{{ URL::asset('assets/backend/js/notification/SmartNotification.min.js') }}"></script>
 		
-		<!-- JARVIS WIDGETS
-		<script src="{{ URL::asset('assets/backend/js/smartwidgets/jarvis.widget.min.js') }}"></script> -->
-		
-		<!-- EASY PIE CHARTS
-		<script src="{{ URL::asset('assets/backend/js/plugin/easy-pie-chart/jquery.easy-pie-chart.min.js') }}"></script> --> 
-		
-		<!-- SPARKLINES
-		<script src="{{ URL::asset('assets/backend/js/plugin/sparkline/jquery.sparkline.min.js') }}"></script> -->
-		
-		<!-- JQUERY MASKED INPUT
-		<script src="{{ URL::asset('assets/backend/js/plugin/masked-input/jquery.maskedinput.min.js') }}"></script> -->
-		
-		<!-- JQUERY SELECT2 INPUT
-		<script src="{{ URL::asset('assets/backend/js/plugin/select2/select2.min.js') }}"></script> -->
-		
-		<!-- JQUERY UI + Bootstrap Slider
-		<script src="{{ URL::asset('assets/backend/js/plugin/bootstrap-slider/bootstrap-slider.min.js') }}"></script> -->
-		
-		<!-- browser msie issue fix 
-		<script src="{{ URL::asset('assets/backend/js/plugin/msie-fix/jquery.mb.browser.min.js') }}"></script>-->
-		
-		<!-- FastClick: For mobile devices
-		<script src="{{ URL::asset('assets/backend/js/plugin/fastclick/fastclick.js') }}"></script> -->
-		
-		<!--[if IE 7]>
-
-		<h1>Your browser is out of date, please update your browser by going to www.microsoft.com/download</h1>
-
-		<![endif]-->
-		{{-- <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script> --}}
 		<script src="{{ URL::asset('assets/backend/js/toastr/toastr.js') }}"></script>
-		
-		<!-- Demo purpose only
-		<script src="{{ URL::asset('assets/backend/js/demo.js') }}"></script>-->
 		
 		<!-- MAIN APP JS FILE-->
 		<script src="{{ URL::asset('assets/backend/js/app.js') }}"></script> 
